7a9617dcff4deb8a2306ff384cba0b34e56ea719
Gestalt <sir.author.doyle@gmail.com>
2021-03-25 17:04:02 -0700

    adding npcs

--- main.py
+++ main.py
@@ -11,17 +11,16 @@ with open('data_lists.json') as f:
 def make_many_npcs(num):
     genders = ["Male", "Female", "Enby"]
     for i in data["cultures"]:
-        for x in range(num):
-            try:
-                for y in range(3):
-                    npc = NPC()
-                    npc.quick_gen(culture=i, gender=genders[y])
-                    print("Sucess! new: " + npc.gender + " " + npc.culture + " - created: " + npc.name + " " + npc.surname)
-                    npc.save_data()
-            except:
-                print("Something went wrong")
-            finally:
-                time.sleep(3)
+        for y in range(3):
+            for x in range(num):
+                try:
+                        npc = NPC()
+                        npc.quick_gen(culture=i, gender=genders[y])
+                        npc.save_data()
+                except:
+                    print("Something went wrong")
+                finally:
+                    time.sleep(3)
 
 
 def make_many_culture(num, culture):
@@ -29,7 +28,6 @@ def make_many_culture(num, culture):
         try:
             npc = NPC()
             npc.quick_gen(culture= culture)
-            print("Sucess! new: " + npc.gender + " " + npc.culture + " - created: " + npc.name + " " + npc.surname)
             npc.save_data()
         except:
             print("Something went wrong")
--- npcs/walk_dir.py
+++ npcs/walk_dir.py
@@ -9,13 +9,15 @@ from NPC import NPC
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
-for root, dirs, files in os.walk(dir_path):
-    for file in files:
-        npc = NPC()
+def rename():
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            npc = NPC()
 
         # change the extension from '.mp3' to
         # the one of your choice.
-        if file.endswith('.json'):
-            npc.load(root + '/' + str(file))
-            new_name = npc.race + " - " + npc.name + "_" + npc.surname + ".json"
-            os.rename(root + '/' + str(file), root + '/' + str(new_name))
+            if file.endswith('.json'):
+                npc.load(root + '/' + str(file))
+                new_name = npc.race + " - " + npc.name + "_" + npc.surname + ".json"
+                os.rename(root + '/' + str(file), root + '/' + str(new_name))
+
